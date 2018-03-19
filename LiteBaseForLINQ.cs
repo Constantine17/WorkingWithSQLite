d201fddd7b4a7724619257c0f0aec617278b9e88
Btech <Cherkashyn@btbpm.com>
2018-03-19 16:58:28 +0200

    SQLite like ADO.NET

---
+++ LiteBaseForLINQ.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithSQLite
+{
+    class LiteBaseForLINQ : LiteBase
+    {
+        public LiteBaseForLINQ(string directory) : base(directory)
+        {
+        }
+
+        public void Craete()
+        {
+            Console.WriteLine(this.directory);
+        }
+
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Data.Common;
+using System.Data.SQLite;
+using System.Data;
+
+
 
 namespace WorkingWithSQLite
 {
@@ -10,6 +16,22 @@ namespace WorkingWithSQLite
     {
         static void Main(string[] args)
         {
+            var lbase = new LiteBase(@"F:\project\WorkingWithSQLite\WorkingWithSQLite\DB\db1.3bd");
+            var lbaseLinq = new LiteBaseForLINQ(@"F:\project\WorkingWithSQLite\WorkingWithSQLite\DB\db1.3bd");
+            string str = "";
+            lbaseLinq.Craete();
+            //lbase.Craete();
+            //lbase.Insert();
+            //lbase.Select();
+            //using (var db = new SQLiteConnection(@"F:\project\WorkingWithSQLite\WorkingWithSQLite\DB\db1.3bd"))
+            //{
+            //    var vr = db.CreateCommand();
+            //    vr.ExecuteNonQuery()
+            //}
+            //Console.WriteLine(Convert.ToInt64(Guid.NewGuid().ToByteArray()));
+            
         }
+
+
     }
 }
